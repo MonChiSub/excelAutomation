a5e9da4129e0770815fe18bf973a198197279d21
MonChiSub <41806105+MonChiSub@users.noreply.github.com>
2021-11-12 09:50:07 +0000

    working version pog

--- src/test/java/selenium/StepDefinitions.java
+++ src/test/java/selenium/StepDefinitions.java
@@ -9,30 +9,77 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class StepDefinitions {
-    WebDriver driver; 
-    private String employeeFirstName, employeeLastName, employeePrevJobTitle, employeePrevJobCompanyName, employeePrevJobStartDate,
-            employeePrevJobEndDate, refTitle, refFirstName, refLastName, refEmail = "";
-    private ArrayList<String> testArray = new ArrayList<>();
+    private static ArrayList<String> fullDatas = new ArrayList<>();
+    private static ArrayList<String> employeeFirstNames = new ArrayList<>();
+    private static ArrayList<String> employeeLastNames = new ArrayList<>();
+    private static ArrayList<String> employeeEmails = new ArrayList<>();
+    private static ArrayList<String> employeePrevJobTitles = new ArrayList<>();
+    private static ArrayList<String> employeePrevJobCompanyNames = new ArrayList<>();
+    private static ArrayList<String> employeePrevJobStartDates = new ArrayList<>();
+    private static ArrayList<String> employeePrevJobEndDates = new ArrayList<>();
+    private static ArrayList<String> refTitles = new ArrayList<>();
+    private static ArrayList<String> refFirstNames = new ArrayList<>();
+    private static ArrayList<String> refLastNames = new ArrayList<>();
+    private static ArrayList<String> refEmails = new ArrayList<>();
+    private String keysPressed =  Keys.chord(Keys.CONTROL, Keys.RETURN);
+    WebDriver driver;
+
 
     @Given("The employee has provided their details to me")
     public void detailCheck() throws IOException {
+        //Method to read the excel sheet and compile the information.
+        compileDetails();
+
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--user-data-dir=C:\\Users\\patri\\AppData\\Local\\Google\\Chrome\\User Data");
         driver = new ChromeDriver(options);
-        driver.get("");
+        driver.get("https://outlook.office.com/mail/inbox");
         driver.manage().window().maximize();
+    }
 
+    @When("I am writing an email for the reference")
+    public void writeEmail() throws InterruptedException {
+        for(int x = 0; x < employeeFirstNames.size(); x++) {
+            //Click Compose
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div[1]/div[1]/div[2]/div/div/button/span/span/span")).click();
+            Thread.sleep(5000);
+            //Type Recipient Email
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div[1]/div/div/div[2]/div[2]/input")).sendKeys(refEmails.get(x));
+            Thread.sleep(3000);
+            //Type the Subject
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div/div/input")).sendKeys("Hello test test 123 xd");
+            Thread.sleep(2000);
+            //Type the message
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[2]/div/div/div[1]"))
+                    .sendKeys("Hello, " + refTitles.get(x) + " " + refLastNames.get(x) + "\n\n"
+                            + "I am writing to you today to ask for a reference on behalf of " + employeeFirstNames.get(x) + " " + employeeLastNames.get(x)
+                            + ", who has claimed to have previously worked for you from the years of " + employeePrevJobStartDates.get(x)
+                            + " - " + employeePrevJobEndDates.get(x) + ", as a " + employeePrevJobTitles.get(x) + " at " + employeePrevJobCompanyNames.get(x)
+                            + ".\n\n" + "Kind Regards, \nThomas Hooson");
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[3]/div[2]/div[1]/div/div/span/button[1]/span/span"));
+        }
+    }
+
+    @Then("Send the email to the recipient")
+    public void sendEmail() throws InterruptedException {
+        driver.quit();
+    }
+
+    public void compileDetails() throws IOException {
         FileInputStream fis = new FileInputStream(new File("C://Users//patri//Desktop//tsiEmployeeInfo.xlsx"));
         XSSFWorkbook workbook = new XSSFWorkbook(fis); // XSSFWorkbook for .xlsx file
         XSSFSheet sheet = workbook.getSheetAt(1); // open sheet 1
@@ -42,70 +89,45 @@ public class StepDefinitions {
         while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
 
-            if (row.getRowNum() == 1) // skip title row
-            {
+            if (row.getRowNum() != 0) {
                 Iterator cellIterator = row.cellIterator();
+
                 while (cellIterator.hasNext()) {
                     Cell cell = (Cell) cellIterator.next();
-                    testArray.add(cell.getStringCellValue());
-                }
-
-                for(int x = 0; x < testArray.size(); x++) {
-                    testArray.get(x);
-                    switch (x) {
-                        default:
-                            break;
-                        case 0:
-                            employeeFirstName = testArray.get(x);
-                            break;
-                        case 1:
-                            employeeLastName = testArray.get(x);
-                            break;
-                        case 3:
-                            employeePrevJobTitle = testArray.get(x);
-                            break;
-                        case 4:
-                            employeePrevJobCompanyName= testArray.get(x);
-                            break;
-                        case 5:
-                            employeePrevJobStartDate = testArray.get(x);
-                            break;
-                        case 6:
-                            employeePrevJobEndDate = testArray.get(x);
-                            break;
-                        case 7:
-                            refTitle = testArray.get(x);
-                            break;
-                        case 8:
-                            refFirstName = testArray.get(x);
-                            break;
-                        case 9:
-                            refLastName = testArray.get(x);
-                            break;
-                        case 10:
-                            refEmail = testArray.get(x);
-                            break;
+                    cell.setCellType(Cell.CELL_TYPE_STRING);
+                    if (!cell.getStringCellValue().isEmpty() &&
+                            !cell.getStringCellValue().equals("0") &&
+                            !cell.getStringCellValue().equals(",")) {
+                        fullDatas.add(cell.getStringCellValue());
                     }
                 }
             }
         }
-    }
-
-    @When("I am writing an email for the reference")
-    public void writeEmail() {
-        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/div[1]/div[2]/div/div/button")).click();
-        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div[1]/div/div/div[2]/div[2]/input")).sendKeys(refEmail);
-        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div"))
-                .sendKeys("Hello, " + refTitle + " " + refLastName + "\n\n"
-                + "I am writing to you today to ask for a reference on behalf of " + employeeFirstName + " " + employeeLastName
-                        + ", who has claimed to have previously worked for you from the years of " + employeePrevJobStartDate
-                        + " - " + employeePrevJobEndDate + ", as a " + employeePrevJobTitle + " at " + employeePrevJobCompanyName
-                        + ".\n\n" + "Kind Regards, \nThomas Hooson");
-    }
-
-    @Then("Send the email to the recipient")
-    public void sendEmail() throws InterruptedException {
-//        driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/div[2]/div/div/div/div/div[2]/div/div[3]/div[1]/div/div/div/div[1]/div[3]/div[2]/div[1]/div/div/span/button[1]")).click();
-//        driver.quit();
+        for (int x = 0; x < (fullDatas.size()); x++) {
+            int y = x + 11;
+            if (y % 11 == 0) {
+                employeeFirstNames.add(fullDatas.get(x));
+            } else if (y % 11 == 1) {
+                employeeLastNames.add(fullDatas.get(x));
+            } else if (y % 11 == 2) {
+                employeeEmails.add(fullDatas.get(x));
+            } else if (y % 11 == 3) {
+                employeePrevJobTitles.add(fullDatas.get(x));
+            } else if (y % 11 == 4) {
+                employeePrevJobCompanyNames.add(fullDatas.get(x));
+            } else if (y % 11 == 5) {
+                employeePrevJobStartDates.add(fullDatas.get(x));
+            } else if (y % 11 == 6) {
+                employeePrevJobEndDates.add(fullDatas.get(x));
+            } else if (y % 11 == 7) {
+                refTitles.add(fullDatas.get(x));
+            } else if (y % 11 == 8) {
+                refFirstNames.add(fullDatas.get(x));
+            } else if (y % 11 == 9) {
+                refLastNames.add(fullDatas.get(x));
+            } else if (y % 11 == 10) {
+                refEmails.add(fullDatas.get(x));
+            }
+        }
     }
 }
